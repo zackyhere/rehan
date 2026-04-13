b72244a65ec5091ae8bb4e08fc23e37f6546cabc
zackyhere <zackyy425@gmail.com>
2026-04-13 17:51:05 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'system-ui, sans-serif'
+    }}>
+      Hello World
+    </div>
+  );
+}
